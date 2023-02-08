6ce2fcf5b40aea3a1fb7574b42d082ab994766a2
Ben Aguirre <ben.aguirre03@gmail.com>
2023-02-08 17:36:12 -0600

    loopBooks() 	- Now loops through books to display all the objects created when click "lets see" button

--- main.js
+++ main.js
@@ -31,22 +31,9 @@ function loopBooks() {
     }
 }
 
+
 const testBook = new Book('Harry Potter', 'JK Rowling', '500', 'read')
 myLibrary.push(testBook)
-console.log(myLibrary[4])
 const testBook2 = new Book('Harry d', 'JK', '30', 'read')
 myLibrary.push(testBook2)
 console.log(myLibrary);
-
-
-
-//newBook doesn't return anything, nor does 
-//it pass the newly-created book to the addBookToLibrary 
-//method. You aren't saying what you want to create from.
-
-// Also, just for the record, why would the Book need to know about the Library? It might make sense to create a Library object, that contains a collection property, perhaps. And that Library might have an add method... that would take the thing to add to its own collection.
-
-// Most of dealing with objects is:
-
-// *  "what does this object need to know to perform properly?" 
-// * "how can I communicate or pass this object to others?"
