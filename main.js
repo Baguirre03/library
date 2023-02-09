99feb2369da5e469e6430ab47863a661fd9e030d
Ben Aguirre <ben.aguirre03@gmail.com>
2023-02-09 13:33:21 -0600

    Putting card info on index 	- Correclty puts the book info in different di s with classes on the page when click current "lets see" button

--- main.js
+++ main.js
@@ -26,18 +26,34 @@ function addBookToLibrary(objectHolder) {
 }
 
 function loopBooks() {
-    for (var i = 0; i < myLibrary.length; i++) {
-        let cards = document.querySelector('.cards')
-        var newDiv = document.createElement('div')
-        newDiv.id = 'card'+i;
-        newDiv.classList = 'bookInfo'
-        var newBook = `${myLibrary[i].title} ${myLibrary[i].author} ${myLibrary[i].pages} ${myLibrary[i].read}`
-        newDiv.textContent = newBook
-        console.log(myLibrary[i].title)
-        cards.appendChild(newDiv)
+    for (i = 0; i < myLibrary.length; i++) {
+        let cardsContainer = document.querySelector('.cardsContainer')
+        let card = document.createElement('div')
+
+        let titleInfo = document.createElement('div')
+        titleInfo.textContent = myLibrary[i].title
+        titleInfo.classList.add('title')
+
+        let authorInfo = document.createElement('div')
+        authorInfo.textContent = myLibrary[i].author
+        authorInfo.classList.add('author')
+
+        let pageInfo = document.createElement('div')
+        pageInfo.textContent = myLibrary[i].pages
+        pageInfo.classList.add('author')
+
+        let readInfo = document.createElement('div')
+        readInfo.textContent = myLibrary[i].read
+        readInfo.classList.add('readInfo')
+
+        cardsContainer.appendChild(card)
+        card.classList.add('card')
+        card.appendChild(titleInfo);
+        card.appendChild(authorInfo)
+        card.appendChild(pageInfo)
+        card.appendChild(readInfo)
     }
 }
-
 
 const testBook = new Book('Harry Potter', 'JK Rowling', '500', 'read')
 myLibrary.push(testBook)
